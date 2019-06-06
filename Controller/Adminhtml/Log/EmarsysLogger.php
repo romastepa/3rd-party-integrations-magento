26ffdcfa7bf4ed946220d9007013caa71a50eb5a
Roman Stepanov <mail2punk@gmail.com>
2019-06-06 15:07:07 +0300

    Logs

--- Controller/Adminhtml/Log/EmarsysLogger.php
+++ Controller/Adminhtml/Log/EmarsysLogger.php
@@ -29,7 +29,7 @@ class EmarsysLogger extends Action
      */
     public function __construct(
         Context $context,
-        Customer $logsResourceModel
+        Logs $logsResourceModel
     ) {
         parent::__construct($context);
         $this->logsResourceModel = $logsResourceModel;
